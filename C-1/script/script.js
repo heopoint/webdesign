5ef3033ba6e4a4b6057e2b48459dec9e47a8f978
heopoint <36278561+heopoint@users.noreply.github.com>
2025-01-30 14:58:31 +0900

    폴더명 변경

---
+++ C-1/script/script.js
@@ -0,0 +1,56 @@
+  // menu
+  $('.main-menu').mouseenter(function () {
+    $(this).find('.sub-menu').stop().slideDown()
+  })
+  $('.main-menu').mouseleave(function () {
+    $(this).find('.sub-menu').stop().slideUp()
+  })
+  // -------------------------------------------
+  $('.slide').eq(0).siblings().hide()
+
+  let idx = 0
+  let slideInterval;
+
+  function startSlide() {
+    clearInterval(slideInterval); // 기존 interval 정지
+    slideInterval = setInterval(function () {
+      idx++
+
+      if (idx === 3) {
+        idx = 0
+      }
+
+      $('.slide').fadeOut()
+      $('.slide').eq(idx).fadeIn()
+
+    }, 3000);
+  }
+
+  // focus 이벤트와 슬라이드 시작 연결
+  $(window).on('focus', startSlide);
+
+  // 초기 슬라이드 실행
+  startSlide();
+  // -------------------------------------------
+  // tab
+  $('.tab-menu a').click(function (e) {
+    e.preventDefault
+
+    let href = $(this).attr('href')
+
+    $('.tab-menu a').parent('li').removeClass('on')
+    $(this).parent('li').addClass('on')
+
+    $('.tab').hide()
+    $(href).show()
+  })
+
+  // popup
+  $('.modal').click(function () {
+    $(this).addClass('on')
+    $('.popup').fadeIn(300)
+  })
+  $('.close').click(function () {
+    $('.modal').removeClass('on')
+    $('.popup').fadeOut(100)
+  })
