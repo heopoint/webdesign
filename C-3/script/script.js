addb9900777aa5aab10f0e8b1a26ac7a8d3db036
heopoint <36278561+heopoint@users.noreply.github.com>
2025-02-02 19:02:03 +0900

    c3

---
+++ C-3/script/script.js
@@ -0,0 +1,50 @@
+  // menu
+  $('.main-menu').mouseenter(function () {
+    $(this).find('.sub-menu').stop().slideDown()
+  })
+  $('.main-menu').mouseleave(function () {
+    $(this).find('.sub-menu').stop().slideUp()
+  })
+  // -------------------------------------------
+ 
+
+  let slideInterval;
+
+  function startSlide() {
+    clearInterval(slideInterval); // 기존 interval 정지
+    slideInterval = setInterval(function () {
+      $('.slide-wrap').animate({ 'left': '-100%' }, function () {
+        $('.slide:first').appendTo('.slide-wrap');
+        $('.slide-wrap').css({ 'left': '0' });
+      });
+    }, 3000);
+  }
+
+  // focus 이벤트와 슬라이드 시작 연결
+  $(window).on('focus', startSlide);
+
+  // 초기 슬라이드 실행
+  startSlide();
+  // -------------------------------------------
+  // tab
+  $('.tab-menu a').click(function (e) {
+    e.preventDefault
+
+    let href = $(this).attr('href')
+
+    $('.tab-menu a').parent('li').removeClass('on')
+    $(this).parent('li').addClass('on')
+
+    $('.tab').hide()
+    $(href).show()
+  })
+
+  // popup
+  $('.modal').click(function () {
+    $(this).addClass('on')
+    $('.popup').fadeIn(300)
+  })
+  $('.close').click(function () {
+    $('.modal').removeClass('on')
+    $('.popup').fadeOut(100)
+  })
