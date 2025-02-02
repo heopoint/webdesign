01712f9336c880b0e1c72f3781aa57178c84045a
heopoint <36278561+heopoint@users.noreply.github.com>
2025-02-02 20:02:43 +0900

    c3

--- C-3/script/script.js
+++ C-3/script/script.js
@@ -25,26 +25,14 @@
 
   // 초기 슬라이드 실행
   startSlide();
-  // -------------------------------------------
-  // tab
-  $('.tab-menu a').click(function (e) {
-    e.preventDefault
-
-    let href = $(this).attr('href')
-
-    $('.tab-menu a').parent('li').removeClass('on')
-    $(this).parent('li').addClass('on')
-
-    $('.tab').hide()
-    $(href).show()
-  })
+ 
 
   // popup
   $('.modal').click(function () {
-    $(this).addClass('on')
+ 
     $('.popup').fadeIn(300)
   })
   $('.close').click(function () {
-    $('.modal').removeClass('on')
+ 
     $('.popup').fadeOut(100)
   })
